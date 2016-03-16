657b2b28191556ba69a6e98c9a59a6603ce03eed
汤天淇 <TianqiTang@tangtianqideMBP.wv.cc.cmu.edu>
2016-03-16 19:58:10 -0400

    hello

---
+++ index.js
@@ -0,0 +1,25 @@
+function my_first_js_function() {
+    var taco=1;
+    var t="abs";
+    var k=[1,2,3];
+
+    if (k==12) {
+        return 42
+    }
+    for (var i=0; i<10; i++) {
+
+    }
+
+
+    return 42;
+}
+
+function kosbie_face_click_handler(){
+    alert("ouch");
+}
+function on_page_ready() {
+    $(".kosbiesface").click(kosbie_face_click_handler)
+}
+
+
+$(document).ready(on_page_ready)
